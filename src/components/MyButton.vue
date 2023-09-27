7a9de00e336505b4eb70c990cafe20db42937a64
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2023-09-27 09:12:37 +0200

    components Button + variant

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -1,13 +1,49 @@
 <script>
+export default {
+  props: {
+    content : String,
+    href : String,
+    size : String,
+    variant : String,
+  },
+  data(){
+    return {
+      className : `button ${this.variant === "rounded" ? " -rounded" : ""}${this.size === "small" ? " -small" : ""}`
+    }
+  }
+}
 </script>
+
 <template>
-  <button class='button'>
-    test button
-  </button>
+  <a v-if="href" :href="href" :class="className"><slot></slot></a>
+  <button v-else :class='className'><slot></slot></button>
 </template>
 
 <style lang="scss" scoped>
   .button{
     background-color: $primary-color;
+    background: linear-gradient(180deg, $secondary-color 0%, $primary-color 100%);
+    border: none;
+    border-radius: rem(15);
+    box-shadow: 0px rem(4) rem(4) rgba(0,0,0,0.25);
+    color: $white;
+    display: inline-block;
+    font-family: $primary-font-familly;
+    font-size: $regular-font-size;
+    font-weight: 600;
+    padding: rem(33) rem(67);
+    text-decoration: none;
+    word-wrap: break-word;
+    &.-rounded{
+      background: $primary-color;
+      border-radius: rem(37);
+      font-weight: 500;
+      padding: rem(26) rem(64);
+    }
+    &.-small{
+      font-size: $small-font-size;
+      padding: rem(14) rem(29);
+      font-weight: 400;
+    }
   }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,5 +3,6 @@ import MyButton from '@/components/MyButton.vue'
 </script>
 
 <template>
- <MyButton />
+ <MyButton>My Button</MyButton>
+ <MyButton size="small" variant="rounded" href="/about">My Button</MyButton>
 </template>
