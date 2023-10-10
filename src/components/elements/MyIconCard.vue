445eef847e19af7abcd4c3266567081f1a88fabb
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2023-10-10 14:11:15 +0200

    Icons fouchette repas livraison

---
+++ src/components/elements/MyIconCard.vue
@@ -0,0 +1,31 @@
+<script setup>
+import IconFourchette from '../icons/IconFourchette.vue'
+import IconLivraison from '../icons/IconLivraison.vue'
+import IconRepas from '../icons/IconRepas.vue'
+
+
+defineProps({
+  name: String
+})
+</script>
+
+<template>
+  <i class="icon">
+    <IconFourchette v-if="name === 'Fourchette'" />
+    <IconLivraison v-if="name === 'Livraison'" />
+    <IconRepas v-else />
+  </i>
+</template>
+
+<style lang="scss" scoped>
+.icon {
+  align-items: center;
+  background: $white;
+  border-radius: 100%;
+  box-shadow: 0 0 rem(5) 0 $gray;
+  display: inline-flex;
+  height: rem(50);
+  justify-content: center;
+  width: rem(50);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <script setup>
-// import Card from '../components/MyCard.vue'
+import MyIconCard from '../components/elements/MyIconCard.vue';
 </script> 
 
 <template>
 <p>Test brouillon</p>
-<!-- <Card image-alt="@/assets/image-1.png" image-src="test" title="test" description="description test" button-label="test check"></Card> -->
+<MyIconCard></MyIconCard>
 </template>
